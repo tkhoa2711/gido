fe07d4d6a1e0d2483c74acecfaf8fe978353378c
Khoa Le <ltkhoa2711@gmail.com>
2017-05-03 13:56:38 +1000

    initial setup for express

---
+++ config/lib/express.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+
+/**
+ * Initialize the Express application
+ */
+module.exports.init = function () {
+  var app = express();
+  return app;
+};
