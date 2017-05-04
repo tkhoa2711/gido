ac5b355d7beb7d93816c521fc62385423ca734e0
Khoa Le <ltkhoa2711@gmail.com>
2017-05-04 13:46:52 +1000

    #2 setup middleware and view engine for express

--- config/lib/express.js
+++ config/lib/express.js
@@ -1,7 +1,34 @@
 'use strict';
 
 var express = require('express'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  methodOverride = require('method-override'),
   path = require('path');
+
+/**
+ * Initialize middleware for Express app
+ */
+module.exports.initMiddleware = function (app) {
+  // for parsing application/x-www-form-urlencoded and application/json
+  // NOTE: the body parsing middleware should be placed before methodOverride
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  // support for overriding HTTP method such as DELETE and PUT based on X-HTTP-Method-Override header
+  app.use(methodOverride());
+
+  // for parsing cookie
+  app.use(cookieParser());
+};
+
+/**
+ * Configure view engine
+ */
+module.exports.initViewEngine = function (app) {
+  // set view path
+  app.set('views', './');
+};
 
 /**
  * Configure routing on the server side
@@ -20,6 +47,8 @@ module.exports.initModulesServerRoutes = function (app) {
  */
 module.exports.init = function () {
   var app = express();
+  this.initMiddleware(app);
+  this.initViewEngine(app);
   this.initModulesServerRoutes(app);
   return app;
 };
